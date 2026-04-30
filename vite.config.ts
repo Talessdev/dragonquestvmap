e5f73266f190ad213c02c9717419a2ce7d69d58f
Tales <talessdev@gmail.com>
2026-04-29 23:26:10 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  // Ajustado para '/' para garantir que os caminhos das imagens sejam lidos da raiz
   base: "/", 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "src"),
     },
-    dedupe: ["react", "react-dom"],
   },
   build: {
     outDir: "dist",
-    emptyOutDir: true,
-    
     assetsInlineLimit: 0, 
-  }
+  },
 });
